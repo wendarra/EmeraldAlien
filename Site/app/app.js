9a33f55ed662d04fa09bcb64d6895efefce3bd1c
Wendarra <wendarra-technical@yahoo.com.au>
2012-03-06 18:21:44 +1000

    Created home page and a tabbed panel for navigation.

--- Site/app/app.js
+++ Site/app/app.js
@@ -1,11 +1,25 @@
 Ext.application({
-    name: 'Sencha',
+    name: 'App',
 
     launch: function () {
-        Ext.create('Ext.Panel', {
+        Ext.create('Ext.tab.Panel', {
             fullscreen: true,
+            tabBarPosition: 'bottom',
             styleHtmlContent: true,
-            html: 'Hello World'
+            items: [
+                {
+                    title: 'Home',
+                    iconCls: 'home',
+                    html: [
+                        '<img src="http://staging.sencha.com/img/sencha.png" />',
+                        '<h1>Welcome to the touch screen PoC</h1>',
+                        "<p>This application demonstrates the use of HTML5, CSS3 ",
+                        "and Javascript to create touch enabled apps for mobiles, tablets ",
+                        "and laptops</p>",
+                        '<h2>Created with Sencha Touch (2.0.0)</h2>'
+                    ].join("")
+                }
+            ]
         });
     }
 
